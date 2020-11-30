989b381b492c092ae0a1cb8aa20fac48a9ddbf9d
LittllMind <aurelien.tisserand18@gmail.com>
2020-11-30 12:46:37 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    AppareilComponent
   ],
   imports: [
     BrowserModule
