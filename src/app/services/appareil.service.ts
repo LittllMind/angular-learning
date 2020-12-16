e7aff1086451b794846e75432d168a9fc287ea41
LittllMind <aurelien.tisserand18@gmail.com>
2020-12-16 14:29:44 +0100

    bootstrap ok

---
+++ src/app/services/appareil.service.ts
@@ -0,0 +1,18 @@
+export class AppareilService {
+
+  appareils = [
+      {
+        name: 'Machine a laver',
+        status: 'off'
+      },
+      {
+        name: 'Frigo',
+        status: 'off'
+      },
+      {
+        name: 'Ordinateur',
+        status: 'off'
+      }
+    ];
+
+}
